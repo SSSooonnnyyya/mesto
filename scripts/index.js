1f0dd497e567af48e9017e1d6e1d3874239ac1d5
Sonya Bagretsova <bigha@yandex.ru>
2022-11-27 19:37:02 +0300

    feat: добавить html, изображения, js и задать стили

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+
+const profileButton = document.querySelector('.profile__info_edit-botton');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__close-botton');
+let name = document.querySelector('.profile__info_name');
+let job = document.querySelector('.profile__info_character');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_character');
+
+profileButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+
+function openPopup(event) {
+    event.preventDefault();
+    popup.classList.add('popup_opened');
+    jobInput.value = job.textContent;
+    nameInput.value = name.textContent;
+}
+
+function closePopup(event) {
+    event.preventDefault();
+    popup.classList.remove('popup_opened');
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    name.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+    popup.classList.remove('popup_opened');
+}
+
+popup.addEventListener('submit', handleFormSubmit);
